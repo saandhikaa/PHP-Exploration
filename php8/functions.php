324701ebf1c1faa4df3e6ff1f166435fe26ab889
gilang sandhika putra <gilangsandhika_putra@yahoo.com>
2022-02-09 10:34:36 +0700

    Login system -menambahkan program registration.php -menambahkan fungsi registrasi pada functions.php

--- php8/functions.php
+++ php8/functions.php
@@ -124,4 +124,34 @@
                     ";
         return query($query);
     }
+
+    
+    function registration($data){
+        global $conn;
+
+        $username = strtolower(stripslashes($data["username"]));
+        $password = mysqli_real_escape_string($conn, $data["password"]);
+        $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+        // cek ketersediaan username
+        $result = mysqli_query($conn, "SELECT username FROM users WHERE username = '$username'");
+        if (mysqli_fetch_assoc($result)){
+            echo "<script>alert('Username sudah dipakai')</script>";
+            return false;
+        }
+        
+        // cek kesamaan password
+        if ($password !== $password2){
+            echo "<script>alert('Password tidak sama')</script>";
+            return false;
+        }
+
+        // enkripsi password
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        
+        // tambahkan user baru ke database, tabel users
+        mysqli_query($conn, "INSERT INTO users VALUES('', '$username', '$password')");
+        return mysqli_affected_rows($conn);
+    }
+    
 ?>
