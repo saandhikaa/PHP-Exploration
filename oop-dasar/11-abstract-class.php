11a48f7a2665d96405448115dc8665672d29f76d
gilang sandhika putra <gilangsandhika_putra@yahoo.com>
2023-01-23 22:25:02 +0700

    abstract class and interface

---
+++ oop-dasar/11-abstract-class.php
@@ -0,0 +1,88 @@
+<?php
+
+abstract class Produk{
+        private $judul, $penulis, $tahun;
+        
+        protected function __construct($judul="", $penulis="", $tahun=0){
+            $this->judul = $judul;
+            $this->penulis = $penulis;
+            $this->tahun = $tahun;
+        }
+        
+        public function resetJudul($judulbaru){
+            $this->judul = $judulbaru;
+        }
+
+        public function resetPenulis($penulisbaru){
+            $this->penulis = $penulisbaru;
+        }
+
+        public function resetTahun($tahunbaru){
+            $this->tahun = $tahunbaru;
+        }
+
+        abstract public function detail();
+
+        public function infoproduk(){
+            $str = "{$this->judul} ({$this->tahun}) written by {$this->penulis}";
+            return $str;
+        }
+    }
+
+    class manga extends Produk{
+        private $penerbit, $volumes;
+
+        public function __construct($judul="<none>", $penulis="<none>", $penerbit="<none>", $volume=0, $tahun=0){
+            parent::__construct($judul, $penulis, $tahun);
+            $this->penerbit = $penerbit;
+            $this->volumes = $volume;
+        }
+
+        public function detail(){
+            $str = "MANGA: {$this->infoproduk()}, vol.{$this->volumes}, {$this->penerbit}";
+            return $str;
+        }
+    }
+
+    class anime extends Produk{
+        private $pengarah, $studio, $episodes;
+
+        public function __construct($judul, $pengarah, $penulis, $studio, $episode, $tahun){
+            parent::__construct($judul, $penulis, $tahun);
+            $this->pengarah = $pengarah;
+            $this->studio = $studio;
+            $this->episodes = $episode;
+        }
+        public function detail(){
+            $str = "ANIME: {$this->infoproduk()} directed by {$this->pengarah}, eps.{$this->episodes}, Studio {$this->studio}";
+            return $str;
+        }
+    }
+
+    class cetakinfo{
+        public $semuaproduk = [];
+
+        public function tambahproduk (Produk $produk){
+            $this->semuaproduk[] = $produk;
+        }
+
+        public function cetak (){
+            $str = "DAFTAR PRODUK:<br>";
+
+            foreach ($this->semuaproduk as $p){
+                $str .= "- {$p->detail()} <br>";
+            }
+
+            return $str;
+        }
+    }
+
+    $produk1 = new manga("Bocchi the Rock!", "Aki Hamaji", "Houbunsha", 5, 2017);
+    $produk2 = new anime("Bocchi the Rock!", "Keiichiro Saito", "Erika Yoshida", "CloverWorks", 12, 2022);
+    
+    $cetaksemua = new cetakinfo();
+    $cetaksemua->tambahproduk($produk1);
+    $cetaksemua->tambahproduk($produk2);
+
+    echo $cetaksemua->cetak();
+?>
