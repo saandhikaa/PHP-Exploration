7c83cca9a27819703042ec0d202ed63b62ee9027
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-11 06:30:07 +0700

    parsing url

--- php-learning/3-mvc/app/core/App.php
+++ php-learning/3-mvc/app/core/App.php
@@ -7,7 +7,9 @@
         
         public function parseURL() {
             if (isset($_GET['url'])){
-                $url = $_GET['url'];
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
                 return $url;
             }
         }
