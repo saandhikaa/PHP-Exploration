4738ed97ebf7db00e64b091ce7c551c888bf89a7
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-10 09:11:40 +0700

    create parse url

--- php-learning/3-mvc/app/core/App.php
+++ php-learning/3-mvc/app/core/App.php
@@ -1,7 +1,15 @@
 <?php
     class App {
         public function __construct() {
-            echo "App.php ready";
+            $url = $this->parseURL();
+            var_dump($url);
+        }
+        
+        public function parseURL() {
+            if (isset($_GET['url'])){
+                $url = $_GET['url'];
+                return $url;
+            }
         }
     }
 ?>
