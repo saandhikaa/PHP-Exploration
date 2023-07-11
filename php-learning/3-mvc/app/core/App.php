9c73e0d40382f5c26378094b74bf3a164b288193
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-11 17:48:30 +0700

    get method from url

--- php-learning/3-mvc/app/core/App.php
+++ php-learning/3-mvc/app/core/App.php
@@ -16,6 +16,14 @@
             // create instance controller
             require_once "../app/controllers/" . $this->controller . ".php";
             $this->controller = new $this->controller;
+            
+            // get method from url
+            if (isset($url[1])) {
+                if (method_exists($this->controller, $url[1])) {
+                    $this->method = $url[1];
+                    unset($url[1]);
+                }
+            }
         }
         
         public function parseURL() {
