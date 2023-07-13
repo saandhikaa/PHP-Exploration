016ca6c511c37781571a18909ba9d09dcc6d7b2f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-13 10:25:44 +0700

    add bootstrap v5.3.0

--- php-learning/3-mvc/app/views/templates/footer.php
+++ php-learning/3-mvc/app/views/templates/footer.php
@@ -1,2 +1,3 @@
+<script src="http://127.0.0.1:8080/programming-note/php-learning/3-mvc/public/js/bootstrap.js"></script>
 </body>
 </html>
--- php-learning/3-mvc/app/views/templates/header.php
+++ php-learning/3-mvc/app/views/templates/header.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["judul"]; ?></title>
+    <link rel="stylesheet" href="http://127.0.0.1:8080/programming-note/php-learning/3-mvc/public/css/bootstrap.css">
 </head>
     
 <body>
