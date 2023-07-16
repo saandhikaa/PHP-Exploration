03632571315dd032a8f99aaf69d86d37c26794a9
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-16 09:12:47 +0700

    create custom jumbotron at welcome page

--- php-learning/3-mvc/app/views/home/index.php
+++ php-learning/3-mvc/app/views/home/index.php
@@ -1,1 +1,8 @@
-<h1>Selamat datang</h1>
+<div class="p-5 mb-4 mt-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+        <h1>Selamat Datang</h1>
+        <p class="fs-6">
+            Selamat datang di website hasil belajar saya tentang PHP MVC! Di sini, Anda akan menemukan hasil perjalanan belajar saya dalam memahami dan mengimplementasikan arsitektur Model-View-Controller (MVC) pada aplikasi PHP.
+        </p>
+    </div>
+</div>
