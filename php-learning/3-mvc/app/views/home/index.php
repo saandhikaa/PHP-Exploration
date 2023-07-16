629a87d45ae8d2c5496628d6aed5d299fe9bbad3
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-16 13:28:44 +0700

    calling method model and get data user

--- php-learning/3-mvc/app/views/home/index.php
+++ php-learning/3-mvc/app/views/home/index.php
@@ -1,8 +1,11 @@
-<div class="p-5 mb-4 mt-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
-        <h1>Selamat Datang</h1>
+<div class="p-5 my-4 bg-light rounded-3">
+    <div class="container-fluid">
+        <h1 class="mb-4">Selamat Datang!</h1>
         <p class="fs-6">
             Selamat datang di website hasil belajar saya tentang PHP MVC! Di sini, Anda akan menemukan hasil perjalanan belajar saya dalam memahami dan mengimplementasikan arsitektur Model-View-Controller (MVC) pada aplikasi PHP.
+        </p>
+        <p class="fs-6 fw-bold">
+            <?= $data["nama"]; ?>
         </p>
     </div>
 </div>
