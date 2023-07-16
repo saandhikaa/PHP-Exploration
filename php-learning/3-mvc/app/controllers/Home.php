1efb48663fbf9c16fc120d6f2f86f744781e2646
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-16 13:26:18 +0700

    add data user

--- php-learning/3-mvc/app/controllers/Home.php
+++ php-learning/3-mvc/app/controllers/Home.php
@@ -2,11 +2,12 @@
     class Home extends Controller {
         public function index() {
             $data = array(
-                "judul" => "Beranda"
+                "judul" => "Beranda",
+                "nama" => $this->model('User_model')->getUser()
             );
             
             $this->view("templates/header", $data);
-            $this->view("home/index");
+            $this->view("home/index", $data);
             $this->view("templates/footer");
         }
         
