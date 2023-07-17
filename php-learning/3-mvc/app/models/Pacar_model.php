54c99201994a8b67c081ee76594cd277597db7a2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-17 13:06:30 +0700

    change data, get from database

--- php-learning/3-mvc/app/models/Pacar_model.php
+++ php-learning/3-mvc/app/models/Pacar_model.php
@@ -1,25 +1,22 @@
 <?php
     class Pacar_model {
-        private $pacarku = array(
-            array(
-                "nama" => "Dilreba",
-                "ultah" => "3 juni",
-                "negara" => "Cina"
-            ),
-            array(
-                "nama" => "Freya",
-                "ultah" => "13 februari",
-                "negara" => "Indonesia"
-            ),
-            array(
-                "nama" => "Rose",
-                "ultah" => "11 februari",
-                "negara" => "Korea"
-            )
-        );
+        private $dbh;   // datanase handler
+        private $stmt;  // statement 
+        
+        public function __construct() {
+            $dsn = 'mysql:host=127.0.0.1:3306;dbname=phpmvc';  // data source name
+            
+            try {
+                $this->dbh = new PDO($dsn, 'root', '');
+            } catch (PDOException $e) {
+                die($e->getMessage());
+            }
+        }
         
         public function daftarPacar() {
-            return $this->pacarku;
+            $this->stmt = $this->dbh->prepare('SELECT * FROM pacarku');
+            $this->stmt->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
         }
     }
 ?>
