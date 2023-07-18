eae7c0f455edd39147557e9e8dac775d7c04c4f2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-18 10:34:38 +0700

    add method: bind

--- php-learning/3-mvc/app/core/Database.php
+++ php-learning/3-mvc/app/core/Database.php
@@ -25,5 +25,28 @@
         public function query ($query) {
             $this->stmt = $this->dbh->prepare($query);
         }
+        
+        public function bind ($param, $value, $type = null) {
+            if (is_null($type)) {
+                switch (true) {
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;
+                        
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break;
+                        
+                    default:
+                        $type = PDO::PARAM_STR;
+                }
+            }
+            
+            $this->stmt->bindValue($param, $value, $type);
+        }
     }
 ?>
