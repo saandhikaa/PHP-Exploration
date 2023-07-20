151a2d31d7fb87c4db09d3e352fd031e053563aa
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-20 13:25:31 +0700

    update view

--- mvc-application/app/views/talent/detail.php
+++ mvc-application/app/views/talent/detail.php
@@ -1,14 +1,24 @@
 <div class="row mx-3 my-4">
     <h2>Biography</h2>
-    <div class="card w-75">
+    <div class="card w-75 mt-4">
         <div class="card-body">
-            <h5 class="card-title"><?= $data['talent']['name_'] ?></h5>
-            <?php foreach ($data['talent'] as $key => $value): ?>
-                <?php if ($key !== "id" && $key !== "name_"): ?>
-                    <p class="card-text"><?= $key ?>: <?= $value ?></p>
-                <?php endif ?>
-            <?php endforeach ?>
-            <a href="<?= BASEURL ?>/talent" class="btn btn-primary">Back</a>
+            <h5 class="card-title mb-4"><?= $data['talent']['name_'] ?></h5>
+            <?php
+                foreach ($data['talent'] as $key => $value) {
+                    if ($key !== "id" && $key !== "name_") {
+                        $key = ucwords(rtrim($key, '_'));
+                        if ($key === "Birthdate") {
+                            $print = $key . ": " . date("M d, Y", strtotime($value));
+                        } else {
+                            $print = $key . ": " . $value;
+                        }
+            ?>
+                        <p class="card-text mb-2"><?= $print ?></p>
+            <?php
+                    }
+                }
+            ?>
+            <a href="<?= BASEURL ?>/talent" class="btn btn-primary mt-4">Back</a>
         </div>
     </div>
 </div>
