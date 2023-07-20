e8be6965298a3e6f5295a622583ba4ea91b6e765
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-20 14:04:18 +0700

    create about us section

--- mvc-application/app/views/about/index.php
+++ mvc-application/app/views/about/index.php
@@ -1,1 +1,24 @@
-<h1>About Us</h1>
+<section id="about">
+    <h2>About Us - Where Dreams Soar and Creativity Knows No Limits!</h2>
+    <p>Welcome to Astronaut Symphony Entertainment, a celestial realm where the boundaries of imagination are pushed, and the universe of creativity knows no limits. Founded with a passion for redefining entertainment, our agency takes you on an extraordinary journey through the cosmos of talent and innovation.</p>
+
+    <h3>Our Cosmic Vision</h3>
+    <p>At Astronaut Symphony Entertainment, we envision a world where artistry and innovation collide, igniting a celestial explosion of awe-inspiring performances and mesmerizing spectacles. Just as astronauts venture beyond Earth's atmosphere to explore the unknown, we strive to venture beyond conventional entertainment to redefine what's possible.</p>
+
+    <h3>Elevating the Arts to Stellar Heights</h3>
+    <p>We are more than just an entertainment agency; we are the curators of dreams and architects of experiences. With a stellar roster of artists, dancers, musicians, and visionaries, we orchestrate performances that transport you to ethereal dimensions and spark the galaxies of emotions within you.</p>
+
+    <h3>Beyond Gravity - Breaking Boundaries</h3>
+    <p>In our cosmic playground, we dare to break the bounds of what's conventional. Prepare to witness gravity-defying acts that challenge the laws of physics, transcendent melodies that resonate in the cosmos, and avant-garde performances that transport you to the far reaches of creativity.</p>
+
+    <h3>A Symphony of Spectacles</h3>
+    <p>From breathtaking concerts under the glimmering stars to breathtaking dance performances that leave you weightless with wonder, our symphony of spectacles promises to leave you mesmerized. Each event we craft is a unique constellation, woven with passion, precision, and boundless imagination.</p>
+
+    <h3>Unleash Your Inner Luminary</h3>
+    <p>Join us in celebrating the luminary within you. Whether you're an artist yearning to showcase your brilliance or an audience member seeking inspiration, Astronaut Symphony Entertainment is your launchpad to the extraordinary.</p>
+
+    <h3>Embark on an Unforgettable Odyssey</h3>
+    <p>The universe is vast, and the possibilities are infinite. Embark on an unforgettable odyssey with us, and let your dreams soar beyond the stars. Together, we'll reach for the cosmos, dancing amidst the constellations of creativity and making magic that echoes through the ages.</p>
+
+    <p>Welcome to the Astronaut Symphony Universe - Where the Stars Align for You!</p>
+</section>
