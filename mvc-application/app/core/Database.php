a71e4ada7d3c04fff8a62c4083ed6708842b167c
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-20 13:00:09 +0700

    add method: rowCount

--- mvc-application/app/core/Database.php
+++ mvc-application/app/core/Database.php
@@ -62,5 +62,9 @@
             $this->execute();
             return $this->stmt->fetch(PDO::FETCH_ASSOC);
         }
+        
+        public function rowCount() {
+            return $this->stmt->rowCount();
+        }
     }
 ?>
