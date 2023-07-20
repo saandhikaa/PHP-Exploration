9263f2864d7da4387438f1d6514473b3698d9b3b
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-20 13:08:30 +0700

    add method: newTalent

--- mvc-application/app/models/Talent_model.php
+++ mvc-application/app/models/Talent_model.php
@@ -17,5 +17,21 @@
             $this->db->bind('id', $id);
             return $this->db->single();
         }
+        
+        public function newTalent ($data) {
+            $query = "INSERT INTO talent (id, name_, birthdate_, birthplace_, occupation_, nationality_) VALUES (:id, :name_, :birthdate_, :birthplace_, :occupation_, :nationality_)";
+            
+            $this->db->query($query);
+            $this->db->bind('id', $data['id']);
+            $this->db->bind('name_', $data['name']);
+            $this->db->bind('birthdate_', $data['birthdate']);
+            $this->db->bind('birthplace_', $data['birthplace']);
+            $this->db->bind('occupation_', $data['occupation']);
+            $this->db->bind('nationality_', $data['nationality']);
+            
+            $this->db->execute();
+            
+            return $this->db->rowCount();
+        }
     }
 ?>
