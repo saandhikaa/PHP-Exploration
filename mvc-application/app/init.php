0fd5c5c3fe948c3bb641c66ac00f66df27c74f9e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-22 14:46:00 +0700

    use autoloader

--- mvc-application/app/init.php
+++ mvc-application/app/init.php
@@ -1,7 +1,11 @@
 <?php
-    require_once "core/App.php";
-    require_once "core/Controller.php";
-    require_once "core/Database.php";
+    // Autoloader function
+    spl_autoload_register(function($className){
+        $classFile = __DIR__ . '/core/' . $className . '.php';
+        if (file_exists($classFile)) {
+            require_once $classFile;
+        }
+    });
     
     require_once "config/config.php";
 ?>
