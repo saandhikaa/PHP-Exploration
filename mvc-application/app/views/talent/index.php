66481423473ef4788903d777ed4919cc77be38eb
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-23 07:05:03 +0700

    add show flasher

--- mvc-application/app/views/talent/index.php
+++ mvc-application/app/views/talent/index.php
@@ -1,6 +1,7 @@
 <div class="row mx-3 my-4">
     <h2>Talent list</h2>
     <div class="d-grid">
+        <?php Flasher::flash() ?>
         <ul class="list-group">
             <?php foreach ($data['talent'] as $talent): ?>
                 <li class="list-group-item d-flex justify-content-between align-items-start">
