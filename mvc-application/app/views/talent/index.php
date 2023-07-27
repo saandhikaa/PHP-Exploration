cbcfccde7f212be1cb8377ee5651ed371d1d155a
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-27 18:19:43 +0700

    open form

--- mvc-application/app/views/talent/index.php
+++ mvc-application/app/views/talent/index.php
@@ -7,7 +7,7 @@
                 <li class="list-group-item">
                     <?= $tn = $talent['name_'] ?>
                     <a href="<?= BASEURL ?>/talent/delete/<?= $talent['id'] ?>" class="text-decoration-none badge text-bg-danger float-end ms-2" onclick="return confirm('Delete Talent: <?= $tn ?> ?');">delete</a>
-                    <a href="<?= BASEURL ?>/talent/edit/<?= $talent['id'] ?>" class="text-decoration-none badge text-bg-success float-end ms-2">edit</a>
+                    <a href="<?= BASEURL ?>/talent/edit/<?= $talent['id'] ?>" class="text-decoration-none badge text-bg-success float-end ms-2" data-bs-toggle="modal" data-bs-target="#newTalent">edit</a>
                     <a href="<?= BASEURL ?>/talent/detail/<?= $talent['id'] ?>" class="text-decoration-none badge text-bg-primary float-end ms-2">detail</a>
                 </li>
             <?php endforeach ?>
