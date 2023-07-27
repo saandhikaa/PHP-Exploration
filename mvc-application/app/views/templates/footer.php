612e3ced56bafbe70e0e41a47b458319ffd0d4b2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-27 19:29:31 +0700

    include jquery library

--- mvc-application/app/views/templates/footer.php
+++ mvc-application/app/views/templates/footer.php
@@ -1,3 +1,4 @@
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
         <script src="<?= BASEURL ?>/js/bootstrap.js"></script>
         <script src="<?= BASEURL ?>/js/script.js"></script>
     </div>
