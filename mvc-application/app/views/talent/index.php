4263f76665f441fa57e37307210c42745b1246a3
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-27 19:30:33 +0700

    rename element id

--- mvc-application/app/views/talent/index.php
+++ mvc-application/app/views/talent/index.php
@@ -18,12 +18,12 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="newTalent" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="newTalent" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <form action="<?= BASEURL ?>/talent/newtalent" method="post">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">New Talent</h1>
+                    <h1 class="modal-title fs-5" id="modalLabel">New Talent</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
