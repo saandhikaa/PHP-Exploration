b979e4774c4687ae28fe89207e7d90b167b694a8
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-27 20:59:21 +0700

    add function for form label New Talent

--- mvc-application/public/js/script.js
+++ mvc-application/public/js/script.js
@@ -1,4 +1,8 @@
 $(function() {
+    $('.formNewTalent').on('click', function() {
+        $('#modalLabel').html('New Talent');
+    });
+    
     $('.formEditTalent').on('click', function() {
         $('#modalLabel').html('Edit Talent');
     });
