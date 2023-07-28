572db547afa36ec9d3b9d779fa4c2fed7dab6633
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-28 20:51:12 +0700

    add AJAX functionality for editing talents

--- mvc-application/public/js/script.js
+++ mvc-application/public/js/script.js
@@ -5,5 +5,15 @@ $(function() {
     
     $('.formEditTalent').on('click', function() {
         $('#modalLabel').html('Edit Talent');
+        const id = $(this).data('id');
+        $.ajax({
+            url: 'http://127.0.0.1:8080/PHP-Exploration/mvc-application/public/talent/edit', 
+            data: {id : id}, 
+            method: 'post', 
+            dataType: 'json', 
+            success: function(data) {
+                console.log(data);
+            }
+        });
     });
 });
