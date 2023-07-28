b4a5e6c04f107c116c3c45f43e0ace64a7ccf814
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-28 22:32:54 +0700

    add hidden input talent id

--- mvc-application/app/views/talent/index.php
+++ mvc-application/app/views/talent/index.php
@@ -27,6 +27,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
+                    <input type="hidden" name="id" value="<?= $talent['id'] ?>">
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" id="name" name="name" autocomplete="off">
