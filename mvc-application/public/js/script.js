921959260cbff1824c8e1144ca71453a941a0617
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-28 21:15:46 +0700

    add constant BASEURL

--- mvc-application/public/js/script.js
+++ mvc-application/public/js/script.js
@@ -1,3 +1,5 @@
+const BASEURL = 'http://127.0.0.1:8080/PHP-Exploration/mvc-application/public';
+
 $(function() {
     $('.formNewTalent').on('click', function() {
         $('#modalLabel').html('New Talent');
@@ -6,8 +8,9 @@ $(function() {
     $('.formEditTalent').on('click', function() {
         $('#modalLabel').html('Edit Talent');
         const id = $(this).data('id');
+        
         $.ajax({
-            url: 'http://127.0.0.1:8080/PHP-Exploration/mvc-application/public/talent/edit', 
+            url: BASEURL + '/talent/edit', 
             data: {id : id}, 
             method: 'post', 
             dataType: 'json', 
